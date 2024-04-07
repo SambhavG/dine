131ef09272c7d1ad498a825a8cfcd26985054061
SambhavG <sambhavgupta159@gmail.com>
2024-04-06 21:58:45 -0700

    Build to docs

---
+++ docs/_app/immutable/entry/start.BSO0uO10.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DGX45XxE.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs",
+		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/dine' : '',
-			relative: false
+			base: process.env.NODE_ENV === 'production' ? '/dine' : ''
 		}
 	}
 };
